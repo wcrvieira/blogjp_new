c225fe06790658a5fec829ab85220563ebd8c934
drifortunato <adrianofortunatomachado@hotmail.com>
2023-04-20 19:00:01 -0300

    correcaoApp

---
+++ apiBlog/Migrations/20230420215410_Banco.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apiBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class Banco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Leitor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leitor", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Leitor");
+        }
+    }
+}
--- apiBlog/Program.cs
+++ apiBlog/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer("name=ConnectionStrings:path"));
+    options.UseSqlServer("name=ConnectionStrings:Development"));
     
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
